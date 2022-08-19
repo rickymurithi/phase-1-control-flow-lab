fad01eece388e4aeb014a67c9085439fd10d3931
erickmuriithi <erick.muriithi@student.moringaschool.com>
2022-08-19 17:34:49 +0300

    add file

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+  //code!
+  if (distance > 2500) {
+    return 'No can do.'
+  }else if (distance > 2000) {
+    return 'I will gladly take your thirty bucks.'
+  }else if (distance <= 400) {
+    return 'This one is on me!'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  //code!
+  return (city === 'NYC') ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  //code!
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    default:
+      return 'Bye.'
+      break;
+  }
 }
